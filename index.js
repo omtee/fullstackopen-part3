b15b0c0694762900fc742cf8a1255e65d7fd4739
Olli Tikka <olli.tikka@andritz.com>
2021-07-27 15:29:18 +0300

    part3-fullstack-deploy

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ morgan.token('body', (req, res) => {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 let persons = [
   { 
