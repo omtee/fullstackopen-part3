7eedca390a5f05752674f076ae3070f3846847a1
Olli Tikka <olli.tikka@andritz.com>
2021-07-28 13:59:27 +0300

    part3-12 mongo.js

---
+++ mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.o62hy.mongodb.net/phonebook-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (name && number) {
+  const person = new Person({
+    name: name,
+    number: number,
+  })
+
+  person.save().then(result => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+} else {
+  Person.find({}).then(result => {
+    result.forEach(note => {
+      console.log(`${note.name} ${note.number}`)
+    })
+    mongoose.connection.close()
+  })
+}
